f99a74afd98373a992a3582cbcb29169e091397a
Zac Stringham <zlstringham@gmail.com>
2020-05-20 06:12:58 -0700

    Install material-ui

--- src/renderer/components/App.jsx
+++ src/renderer/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import { Typography } from '@material-ui/core'
+import { EmojiPeople } from '@material-ui/icons'
+
 function App () {
   return (
-    <>Hello, Electron+React!</>
+    <>
+      <EmojiPeople />
+      <Typography>Hello, Electron+React!</Typography>
+    </>
   )
 }
 
