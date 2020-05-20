cfd2fdb2bb1501410a6d603521a0f0f6cb5f8148
Zac Stringham <zlstringham@gmail.com>
2020-05-20 04:31:30 -0700

    Configure minimal React app in the renderer process

---
+++ webpack.renderer.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
