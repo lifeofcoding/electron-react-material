421d11355d772a9ba88c0e4a89f6456225e5941d
Zac Stringham <zlstringham@gmail.com>
2020-05-20 04:25:21 -0700

    Configure minimal electron app

---
+++ src/main/index.js
@@ -0,0 +1,42 @@
+import * as path from 'path'
+import { format as formatUrl } from 'url'
+
+import { BrowserWindow, app } from 'electron'
+
+const IS_DEV_MODE = process.env.NODE_ENV !== 'production'
+
+let mainWindow = null
+
+function createMainWindow () {
+  if (mainWindow !== null) return
+
+  mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  if (IS_DEV_MODE) {
+    mainWindow.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
+  } else {
+    mainWindow.loadURL(formatUrl({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file',
+      slashes: true
+    }))
+  }
+
+  mainWindow.on('ready-to-show', mainWindow.show)
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createMainWindow)
+
+// macOS behavior: Don't quit when all windows are closed
+app.on('activate', createMainWindow)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
